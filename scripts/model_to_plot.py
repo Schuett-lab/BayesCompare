542075d8bf1c62364ccc5208bfa824f9ab070fde
SezanMert <sezanmert@gmail.com>
2025-10-24 10:02:38 +0200

    Model to plot pipeline updated for two distances and all ResNet layers.

--- scripts/model_to_plot.py
+++ scripts/model_to_plot.py
@@ -9,7 +9,11 @@ import torch
 import torchvision
 import tqdm
 from sklearn.manifold import MDS
+import scipy
+from torchvision.models.feature_extraction import get_graph_node_names
+import pickle
 
+###------------------------------- For only Wasserstein and a subset of layers of 5 ResNets.
 '''
 ## Compute the covariances from trained models:
 
@@ -116,14 +120,20 @@ np.save("covs_1000_all_resnets_densesampled.npy", np.stack(covs))
 
 ## Directly from saved covariance matrix
 
+'''
 covs = np.load("/home/sezan/Documents/BayesCompare/covs_1000_all_resnets_densesampled.npy")
 
 dist = BayesCompare.measure_dist(covs, checkpoint_dir="/home/sezan/Documents/BayesCompare/my_dist_checkpoints/", meas_name=['wasserstein'], alpha=10/11)
 
 dist = dist['wasserstein']
 
-np.save("dist_wasserstein_corrected_1000_all_resnets_densesampled", dist)
+np.save("dist_wasserstein_corrected_1000_all_resnets_densesampled.npy", dist)
 
+'''
+## Directly from saved distance matrix
+
+'''
+dist = np.load("nonan_symm_dist_wasserstein_corrected_1000_all_resnets_densesampled.npy")
 
 plt.figure()
 plt.imshow(dist, "bone", vmin=0, vmax=np.max(dist))
@@ -143,7 +153,7 @@ plt.savefig("figures/normalized_dist_wasserstein_corrected_allresnets.svg", dpi=
 
 
 mds = MDS(dissimilarity="precomputed")
-mds.fit(normalized_dists)
+mds.fit(dist)
 
 n = 91 # number of layers per model
 m = 5 # number of models
@@ -248,7 +258,222 @@ plt.fill_between(range(n), mean-std, mean+std, alpha=0.15, color='gray')
 plt.xticks(ticks=range(n), labels=model_layers, rotation=90, fontsize=6)
 plt.grid(axis='x', color='gray', alpha=0.1, linewidth=0.5)
 plt.xlabel("Layer")
-plt.ylabel("JSD Distance")
+plt.ylabel("Wasserstein Distance")
 plt.tight_layout()
 plt.savefig("figures/resnet50_results/avg_dist_wasserstein_corrected_corrected_allresnets_densesampled_retrieval.svg", dpi=400)
+'''
 
+###------------------------------- For Wasserstein and JSD and for all layers of all ResNets
+'''
+# Load a checkpoint model
+snapshot1 = torch.load("/home/sezan/Documents/BayesCompare/checkpoints/snapshot_ep99_seed128_iter7.pth")
+snapshot2 = torch.load("/home/sezan/Documents/BayesCompare/checkpoints/snapshot_ep99_seed333_iter8.pth")
+snapshot3 = torch.load("/home/sezan/Documents/BayesCompare/checkpoints/snapshot_ep200_seed122_iter6.pth")
+snapshot4 = torch.load("/home/sezan/Documents/BayesCompare/checkpoints/snapshot_ep200_seed400_iter4.pth")
+
+model1 = torchvision.models.get_model("resnet50", weights=None)
+model2 = torchvision.models.get_model("resnet50", weights=None)
+model3 = torchvision.models.get_model("resnet50", weights=None)
+model4 = torchvision.models.get_model("resnet50", weights=None)
+model5 = torchvision.models.get_model("resnet50", weights=torchvision.models.ResNet50_Weights.IMAGENET1K_V2)
+
+model1.load_state_dict(snapshot1["model_state"])
+model2.load_state_dict(snapshot2["model_state"])
+model3.load_state_dict(snapshot3["model"])
+model4.load_state_dict(snapshot4["model"])
+
+models = [model1, model2, model3, model4, model5]
+
+for model in models:
+    model.eval()
+
+# Load the MS COCO dataset
+im_folder = '/home/sezan/Documents/BayesCompare/images/unlabeled2017'
+file_names = os.listdir(im_folder)
+
+N = 1000
+ims = [PIL.Image.open(
+    os.path.join(im_folder, f_name))
+    for f_name in file_names[:N]
+]
+
+interpolation=tvt.functional.InterpolationMode.BILINEAR
+transform = tvt.Compose([tvt.Resize(232, interpolation=interpolation, antialias=True),
+                         tvt.CenterCrop(224),
+                         tvt.ToTensor(),
+                         tvt.Normalize(
+                         mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225]
+                         )
+                        ])
+
+transformed_ims = [transform(im.convert('RGB')) for im in ims]
+x_input = torch.stack(transformed_ims)
+
+# check the layer names with train_nodes before getting the features
+node_names, _ = get_graph_node_names(model1)
+
+covs = []
+
+with torch.inference_mode():
+    
+    for model in tqdm.tqdm(models):
+        
+        covs_extractor = BayesCompare.cov_extractor(model, return_nodes=node_names)        
+        covs.append(covs_extractor(x_input))
+
+with open('covs_1000_all_resnets_all_layers.pkl', "wb") as f:
+    pickle.dump(covs, f)
+'''
+
+## Directly from saved covariance matrix
+'''
+with open('covs_1000_all_resnets_all_layers.pkl', "rb") as f:
+    covs_names = pickle.load(f)
+
+covs = []
+
+for cov_dict in covs_names:
+    
+    covs.append(list(cov_dict.values()))
+    layer_names = list(cov_dict.keys())
+
+covs = np.stack(covs)
+covs = covs.reshape(covs.shape[0] * covs.shape[1], covs.shape[2], covs.shape[3])
+
+dist = BayesCompare.measure_dist(covs, checkpoint_dir="/home/sezan/Documents/BayesCompare/dist_checkpoints/", meas_name=['wasserstein', 'JSD'], alpha=10/11)
+
+np.save("dist_wasserstein_1000_all_resnets_all_layers.npy", dist['wasserstein'])
+
+np.save("dist_JSD_1000_all_resnets_all_layers.npy", dist['JSD'])
+'''
+## Directly from saved distances
+
+means = []
+stds = []
+
+main_colors = ['steelblue', 'indigo']
+error_colors = ['powderblue', 'thistle']
+
+dist_names = ['wasserstein', 'JSD']
+
+for dist_name in dist_names:
+    
+    dist = np.load("dist_"+dist_name+"_1000_all_resnets_all_layers.npy")
+
+    plt.figure()
+    plt.imshow(dist, "bone", vmin=0, vmax=np.max(dist))
+    plt.colorbar()
+    plt.title(dist_name.capitalize()+" Distance")
+    ax = plt.gca()
+    ax.set_axis_off()
+    plt.savefig("figures/resnet50_results/all_layers/dist_"+dist_name+"_allresnets_alllayers.svg", dpi=800)
+
+    mds = MDS(dissimilarity="precomputed")
+    mds.fit(dist)
+
+    n = int(len(dist)/5) # number of layers per model
+    m = 5 # number of models
+
+    x = mds.embedding_
+    x0 = x[1:n+1, :]
+    x1 = x[n+2:2*n+2, :]
+    x2 = x[2*n+3:3*n+3, :]
+    x3 = x[3*n+4:4*n+4, :]
+    x4 = x[4*n+5:5*n+5, :]
+
+    plt.figure()
+    plt.plot(x0[:, 0], x0[:, 1], '.-', linewidth=2, markersize=10, color="#fa5750")  
+    plt.plot(x1[:, 0], x1[:, 1], '.-', linewidth=2, markersize=10, color="#dbb32d")  
+    plt.plot(x2[:, 0], x2[:, 1], '.-', linewidth=2, markersize=10, color="#4695f7")  
+    plt.plot(x3[:, 0], x3[:, 1], '.-', linewidth=2, markersize=10, color="#33db2d") 
+    plt.plot(x4[:, 0], x4[:, 1], '.-', linewidth=2, markersize=10, color="#d446f7") 
+    plt.arrow(
+        x[0, 0], x[0, 1], x0[0, 0] - x[0, 0], x0[0, 1] - x[0, 1],
+        length_includes_head=True, width=0.01, fc='black', ec=None)
+    plt.arrow(
+        x[0, 0], x[0, 1], x1[0, 0] - x[0, 0], x1[0, 1] - x[0, 1],
+        length_includes_head=True, width=0.01, fc='black', ec=None)
+    plt.arrow(
+        x[0, 0], x[0, 1], x2[0, 0] - x[0, 0], x2[0, 1] - x[0, 1],
+        length_includes_head=True, width=0.01, fc='black', ec=None)
+    plt.arrow(
+        x[0, 0], x[0, 1], x3[0, 0] - x[0, 0], x3[0, 1] - x[0, 1],
+        length_includes_head=True, width=0.01, fc='black', ec=None)
+    plt.arrow(
+        x[0, 0], x[0, 1], x4[0, 0] - x[0, 0], x4[0, 1] - x[0, 1],
+        length_includes_head=True, width=0.01, fc='black', ec=None)
+    plt.axis("equal")
+    ax = plt.gca()
+    ax.set_axis_off()
+    plt.savefig("figures/resnet50_results/all_layers/dist_"+dist_name+"_allresnets_alllayers_mds.svg")
+    plt.savefig("figures/resnet50_results/all_layers/dist_"+dist_name+"_allresnets_alllayers_mds.pdf")
+
+    plt.figure(figsize=(17,7), dpi=400)
+
+    blocks = dist.reshape((m, n, m, n)).transpose(0,2,1,3).reshape(m*m,n,n)
+    all_idx = np.triu(np.arange(m*m).reshape(m,m),k=1)
+    idx = all_idx[np.where(all_idx!=0)]
+
+    model_names = ["1 2", "1 3", "1 4", "1 5", "2 3", "2 4", "2 5", "3 4", "3 5", "4 5"]
+
+    model_idx = 0
+
+    cmap = plt.cm.get_cmap('viridis', 10)
+
+    model5 = torchvision.models.get_model("resnet50", weights=torchvision.models.ResNet50_Weights.IMAGENET1K_V2)
+    model_layers, _ = get_graph_node_names(model5)
+
+    stack_diags = np.zeros((int(m*(m-1)/2), n))
+
+    for i in idx:
+        diags = np.diag(blocks[i])
+        stack_diags[model_idx, :] = diags
+        plt.plot(range(n), diags, '.-', linewidth=1, markersize=5, label="Model "+model_names[model_idx][0]+" vs Model "+model_names[model_idx][2], color=cmap(model_idx))
+        model_idx += 1
+        
+    plt.xlabel("Layer")
+    plt.xticks(ticks=range(n), labels=model_layers, rotation=90, fontsize=6)
+    plt.grid(axis='x', color='gray', alpha=0.3, linewidth=0.5)
+    plt.ylabel(dist_name.capitalize()+" Distance")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("figures/resnet50_results/all_layers/dist_"+dist_name+"_allresnets_alllayers_retrieval.svg", dpi=400)
+    
+    mean = stack_diags.mean(axis=0)
+    var = stack_diags.var(axis=0, ddof=1)
+    std = np.sqrt(var)
+    
+    means.append(mean)
+    stds.append(std)
+
+    plt.figure(figsize=(14, 6), dpi=400)
+
+    plt.plot(range(n), mean, '-o', markersize=2, linewidth=1, color='black')
+    plt.fill_between(range(n), mean-std, mean+std, alpha=0.15, color='gray')
+    plt.xticks(ticks=range(n), labels=model_layers, rotation=90, fontsize=6)
+    plt.grid(axis='x', color='gray', alpha=0.1, linewidth=0.5)
+    plt.xlabel("Layer")
+    plt.ylabel(dist_name.capitalize()+" Distance")
+    plt.tight_layout()
+    plt.savefig("figures/resnet50_results/all_layers/avg_dist_"+dist_name+"_allresnets_alllayers_retrieval.svg", dpi=400)
+
+# compare the averages
+
+plt.figure(figsize=(14, 6), dpi=400)
+
+for j, (mean_arr, std_arr) in enumerate(zip(means, stds)):
+    x = np.arange(len(mean_arr))
+    plt.plot(x, mean_arr, '-o', markersize=2, linewidth=1,
+             color=main_colors[j], label=dist_names[j], zorder=3 + j)
+    plt.fill_between(x, mean_arr - std_arr, mean_arr + std_arr,
+                     alpha=0.15, color=error_colors[j], zorder=1 + j)
+
+xtick_count = len(model_layers)
+plt.xticks(ticks=range(xtick_count), labels=model_layers, rotation=90, fontsize=6)
+plt.grid(axis='x', color='gray', alpha=0.1, linewidth=0.5)
+plt.xlabel("Layer")
+plt.ylabel("Distance")
+plt.legend()
+plt.tight_layout()
+plt.savefig("figures/resnet50_results/all_layers/avg_dist_comparison_allresnets_alllayers_retrieval.svg", dpi=400)
