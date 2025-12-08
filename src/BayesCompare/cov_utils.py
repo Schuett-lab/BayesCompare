d2a8f387bdd96890b05b2095a6c5e7ef43666556
Hororohoruru <juanjesustorre@gmail.com>
2025-12-08 10:27:38 +0100

    Added cov_utils module for covariance operations

---
+++ src/BayesCompare/cov_utils.py
@@ -0,0 +1,61 @@
+"""
+Module with helper functions to manipulate covariance matrices
+Authors: Juan Jesús Torre Tresols, Sezan Oral, Heiko Schütt
+"""
+
+import numpy as np
+
+from numpy.typing import NDArray
+
+
+def trace_norm(cov: NDArray) -> NDArray:
+    """Normalize the covariance to have trace equal to its shape"""
+
+    cov_norm = cov * cov.shape[0] / np.trace(cov)
+
+    return cov_norm
+
+
+def cov_sigma(
+    cov: NDArray, noise_var: float, signal_var: float | None = None
+) -> NDArray:
+    """
+    Multiply the (normalized) covariance matrix by the estimated variance of
+    the signal, and add the estimated variance of the noise, so the resulting
+    matrix represents the estimated total variance of the predictive distribution
+
+    Parameters
+    ----------
+
+    cov: np.array, shape (n_stim, n_stim)
+        Covariance matrix
+
+    noise_var: float
+        Estimate of the variance attributed to noise
+
+    signal_var: float or None, default None
+        Estimate of the variance attributed to the data. If not passed, it is
+        assumed to be (1 - noise_var)
+
+    Returns
+    -------
+
+    cov_sigma: NDArray
+        Modified covariance matrix that represents the variance of the
+        predictive distribution of y
+    """
+    N = cov.shape[0]
+
+    cov_sigma = signal_var * cov + noise_var * np.eye(N)
+
+    return cov_sigma
+
+
+def _check_cov_normalized(cov, tolerance=1e-4):
+    """
+    Check if the given covariance is trace normalized
+    """
+
+    trace_cov = covs[idx].trace()
+
+    return abs(trace_cov - len(covs[idx])) < tolerance
