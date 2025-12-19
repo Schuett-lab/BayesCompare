cc969eb2642d426160814a674120bac29a1df312
SezanMert <sezanmert@gmail.com>
2025-12-19 16:24:55 +0100

    Changing the directory of the `test_meas_dist_parallel`

--- tests/test_meas_dist_parallel.py
+++ tests/test_meas_dist_parallel.py
@@ -7,28 +7,28 @@ import h5py
 
 ## Compute the distance
 
-input_dir = '/home/sezan/Documents/BayesCompare/covs_1000_normalized.npy'
-#input_dir = '/home/sezan/Documents/BayesCompare/covs_1000.npy'
-#input_dir = '/home/sezan/Documents/BayesCompare/covs_1000_resnet50_densesampled_normalized.npy'
-   
-output_dir = '/home/sezan/Documents/BayesCompare/parallel_tests_outs'
+input_dir = "/home/sezan/Documents/BayesCompare/covs_1000_normalized.npy"
+# input_dir = '/home/sezan/Documents/BayesCompare/covs_1000.npy'
+# input_dir = '/home/sezan/Documents/BayesCompare/covs_1000_resnet50_densesampled_normalized.npy'
 
-mdp.measure_dist_parallel(input_dir, output_dir, meas_name='JSD', num_workers=20)
+output_dir = "/home/sezan/Documents/BayesCompare/parallel_tests_outs"
+
+mdp.measure_dist_parallel(input_dir, output_dir, meas_name="JSD", num_workers=20)
 
 ## Check the calculated distance matrix
 
 filename = "/home/sezan/Documents/BayesCompare/parallel_tests_outs/dist_covs_1000_normalized_JSD.hdf5"
 
 with h5py.File(filename, "r") as f:
-    
+
     f.visititems(lambda name, obj: print(f"  {name}: {type(obj)}"))
-    
+
     dset = f["dist"]
     data = dset[...]
 
 for i in range(len(data)):
     data[i, i] = 0.0
-        
+
 plt.figure()
 plt.imshow(data, "bone", vmin=0, vmax=np.max(data))
 plt.colorbar()
@@ -39,16 +39,16 @@ plt.show()
 
 ## Compute distance with the non-parallized method
 
-input_dir = '/home/sezan/Documents/BayesCompare/covs_1000_normalized.npy'
+input_dir = "/home/sezan/Documents/BayesCompare/covs_1000_normalized.npy"
 
 covs = np.load(input_dir)
 
 
-N = len(covs)   
+N = len(covs)
 
 dist = np.zeros((len(covs), len(covs)))
 
-progress_bar = tqdm.tqdm(total=int((N*(N-1))/2))
+progress_bar = tqdm.tqdm(total=int((N * (N - 1)) / 2))
 
 for i, ci in enumerate(covs):
 
@@ -68,4 +68,3 @@ plt.colorbar()
 ax = plt.gca()
 ax.set_axis_off()
 plt.show()
-
