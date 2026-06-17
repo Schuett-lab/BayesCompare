376f3470bc7d437e6b1b5b55a36051ef70867a86
SezanMert <sezanmert@gmail.com>
2026-06-17 12:38:33 +0200

    Clean up of the old scripts and obsolete source code for distances and model-to-model comparison analyses. Changed `__init__` accordingly.

--- src/BayesCompare/__init__.py
+++ src/BayesCompare/__init__.py
@@ -1,10 +1,6 @@
 from .dnn_extract import get_cov
 from .dnn_extract import cov_extractor
 from .dnn_extract import get_layer_names
-from .normals import jsd_normal_sig
-from .normals import jsd_normal_general
-from .normals import tvd_normal_sig
-from .normals import tvd_normal_general
 from .cov_utils import cov_sigma
 from .cov_utils import trace_norm
 from .inference import inference
@@ -12,4 +8,3 @@ from .inference import evidence
 from .inference import inference_cov
 from .distances import measure_dist
 from .meas_dist_parallel import measure_dist_parallel
-from . import others
