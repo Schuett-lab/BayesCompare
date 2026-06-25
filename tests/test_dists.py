c5e4eaef206137c9d326bc019e9f84fd42f19e10
SezanMert <sezanmert@gmail.com>
2026-06-25 15:06:59 +0200

    Increased tolerance for TVD in `test_dists` for CI errors.

--- tests/test_dists.py
+++ tests/test_dists.py
@@ -15,7 +15,6 @@ ALL_MEASURES = [
     "bhattacharyya",
     "mahalanobis",
 ]
-
 # number of samples for computing jsd and tvd distances.
 # can also be set separate for each test or globally from here
 # highly affects how long the tests take
@@ -183,7 +182,7 @@ def test_similar_input(inputs, meas_name):
         assert_allclose(output_np, np.zeros_like(output_np), rtol=0.1, atol=0.1)
         assert_close(output_th, torch.zeros_like(output_th), rtol=0.1, atol=0.1)
         assert_allclose(output_np, output_th.numpy(), rtol=1e-3, atol=1e-3)
-    elif "jsd" in meas_name:
+    elif "jsd" in meas_name or "tvd" in meas_name:
         assert_allclose(output_np, np.zeros_like(output_np), rtol=0.05, atol=0.05)
         assert_close(output_th, torch.zeros_like(output_th), rtol=0.05, atol=0.05)
         assert_allclose(output_np, output_th.numpy(), rtol=1e-2, atol=1e-2)
