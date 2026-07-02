1060c07f69e194de95150138f5a8153f95fa4221
Hororohoruru <juanjesustorre@gmail.com>
2026-07-02 11:53:38 +0200

    Removed unused inference and inference_cov functions from encoding.py

--- src/BayesCompare/encoding.py
+++ src/BayesCompare/encoding.py
@@ -1,103 +1,13 @@
 import numpy as np
 
 from joblib import Parallel, delayed
-from scipy.linalg import pinv
 from scipy.special import logsumexp
 from typing import Tuple, Optional
 from tqdm import tqdm
 
 from numpy.typing import NDArray
 
-from .cov_utils import cov_sigma
-
-
-def inference(cov, y_train, alpha: float = 0.1):
-    """calculates bayesian inference for the 0 mean Gaussian prediction
-    with covariance cov and training data y_train.
-
-    input:
-        cov (2d array): covariance across *all* relevant stimuli
-            the first len(y_train) are assumed to correspond to
-            the observations y_train. The rest we make predictions for.
-            This should be the covariances after scaling,
-            but before adding noise
-        y_train(1d array): data to fit to.
-        alpha (float): additional noise variance to add , default = 0
-            if the covariances are not invertible, this is necessary!
-
-    output:
-        y_mu (vector): The mean prediction for the test stimuli, i.e. all
-            stimuli that are not training stimuli
-        y_sigma (2d array): covariance between test stimulus predictions
-            This does not include any noise covariance.
-    """
-    N = len(y_train)
-    N_pred = cov.shape[0] - N
-    # inner_inv = np.linalg.inv(sigma_e * np.eye(N) + cov[:N, :N])
-    inner_inv = np.linalg.inv(alpha / (1 - alpha) * np.eye(N) + cov[:N, :N])
-    # this is wrong for sigma_e = 0
-    # y_mu = np.matmul(
-    #    cov[N:(N+N_pred), :N] - np.matmul(
-    #        cov[N:(N+N_pred), :N],
-    #        np.matmul(inner_inv, cov[:N, :N])),
-    #    y_train) / sigma_e
-    y_mu = (
-        (
-            cov[N : (N + N_pred), :N]
-            - cov[N : (N + N_pred), :N] @ inner_inv @ cov[:N, :N]
-        )
-        * (1 - alpha)
-        / alpha
-        @ y_train
-    )
-    # covariance is ok, checked!
-    # this is the covariance without noise
-    y_sigma_post = cov[N : (N + N_pred), N : (N + N_pred)] - np.matmul(
-        np.matmul(cov[N : (N + N_pred), :N], inner_inv), cov[:N, N : (N + N_pred)]
-    )
-    return y_mu, y_sigma_post
-
-
-def inference_cov(cov, y_train, alpha: float = 0):
-    """calculates bayesian inference for the 0 mean Gaussian prediction
-    with covariance cov and training data y_train.
-
-    input:
-        cov (2d array): covariance across *all* relevant stimuli
-            the first len(y_train) are assumed to correspond to
-            the observations y_train. The rest we make predictions for.
-            This should be the covariances after scaling,
-            but before adding noise
-        y_train(1d array): data to fit to.
-        alpha (float): additional noise variance to add , default = 0
-            The used covariance is (1-alpha) * cov + alpha * I * trace(cov)
-            if the covariances are not invertible, this is necessary!
-
-    output:
-        y_mu (vector): The mean prediction for the test stimuli, i.e. all
-            stimuli that are not training stimuli
-        y_sigma (2d array): covariance between test stimulus predictions.
-    """
-    N = len(y_train)
-    N_pred = cov.shape[0] - N
-    k11 = cov[:N, :N]
-    k21 = cov[N : (N + N_pred), :N]
-    k22 = cov[N : (N + N_pred), N : (N + N_pred)]
-    if alpha < 1 and alpha > 0:
-        inner_inv = np.linalg.inv(alpha / (1 - alpha) * np.eye(N) + k11)
-        y_mu = (k21 - k21 @ inner_inv @ k11) * (1 - alpha) / alpha @ y_train
-        y_sigma_post = k22 - k21 @ inner_inv @ k21.T
-    elif alpha == 0:  # no noise
-        # murphy kernel trick (17.108)
-        inner_inv = pinv(k11)
-        y_mu = k21 @ inner_inv @ y_train
-        y_sigma_post = k22 - k21 @ inner_inv @ k21.T
-    elif alpha == 1:  # only prior
-        y_mu = np.zeros(N_pred)
-        y_sigma_post = k22
-    else:
-        raise ValueError("alpha must be between 0 and 1")
-    return y_mu, y_sigma_post
+from BayesCompare.cov_utils import cov_sigma
 
 
 def evidence(
