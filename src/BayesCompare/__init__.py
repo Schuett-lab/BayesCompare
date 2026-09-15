707ca99ba2b30b6bec6f170c8df5a645ba2df989
SezanMert <sezanmert@gmail.com>
2026-09-15 14:58:23 +0200

    Removed functions from init and updated the tutorials accordingly.

--- src/BayesCompare/__init__.py
+++
@@ -1,5 +0,0 @@
-from .dnn_extract import cov_extractor
-from .dnn_extract import cov_extractor_batch
-from .dnn_extract import get_layer_names
-from .measure_distance import measure_dist
-from .measure_distance import measure_dist_parallel
